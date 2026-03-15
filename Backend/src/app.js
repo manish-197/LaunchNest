2cfffff451659875ff5df382ab38eb1df6e25d20
MANISH PATIL <manishpatil0738@gmail.com>
2026-03-15 13:01:48 +0530

    done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -29,7 +29,7 @@ app.use('/api/startup', startupRouter)
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.use((req, res, next) => {
