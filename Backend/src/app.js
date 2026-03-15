752658af9005a1a50ba4527a8dd10865a388dfa2
MANISH PATIL <manishpatil0738@gmail.com>
2026-03-15 14:32:07 +0530

    done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,7 @@ const connectDB = require('./config/database')
 const authRouter = require('./routes/auth.routes')
 const startupRouter = require('./routes/startup.routes')
 const cookieParser = require("cookie-parser");
-const path = require('path');
-
-
-
+// const path = require('path');
 
 
 connectDB()
@@ -26,11 +23,8 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter)
 app.use('/api/startup', startupRouter)
 
-app.use(express.static("./public"))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
