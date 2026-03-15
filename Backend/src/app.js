24e38633e43d1c8b18363e4e395e5d3a98124a51
MANISH PATIL <manishpatil0738@gmail.com>
2026-03-15 13:12:06 +0530

    Done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -26,9 +26,15 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter)
 app.use('/api/startup', startupRouter)
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
-app.get('(.*)', (req, res) => {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
