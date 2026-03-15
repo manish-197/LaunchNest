8e55cbaf028ec261807e3f26b4d8d1f8390ce714
MANISH PATIL <manishpatil0738@gmail.com>
2026-03-15 12:41:51 +0530

    done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/database')
 const authRouter = require('./routes/auth.routes')
 const startupRouter = require('./routes/startup.routes')
 const cookieParser = require("cookie-parser");
+const path = require('path');
+
 
 
 
@@ -13,16 +15,23 @@ connectDB()
 const app = express()
 app.use(express.json())
 app.use(cors(
-    {
-  origin: 'http://localhost:5173', 
-  credentials: true,               
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
-}
+  {
+    origin: 'http://localhost:5173', 
+    credentials: true,               
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+  }
 ));
 app.use(cookieParser());
 app.use('/api/auth', authRouter)
 app.use('/api/startup', startupRouter)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   next();
