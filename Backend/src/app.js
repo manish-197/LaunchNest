d1a8722a540dffa5d94a2bdf25b84bae68fa9346
MANISH PATIL <manishpatil0738@gmail.com>
2026-07-29 18:40:08 +0530

    error fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,9 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
   {
-    origin: [
-      "http://localhost:5173",
-    'https://launch-nest.netlify.app' ],
+    origin: 'https://launch-nest.netlify.app',
     credentials: true,               
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
